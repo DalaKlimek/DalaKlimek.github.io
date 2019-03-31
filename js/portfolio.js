e3686fb4280699508a4577afabe84023cd2e1800
DalaKlimek <46930954+DalaKlimek@users.noreply.github.com>
2019-03-31 15:52:19 +0200

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,6 @@
 function zoomIn(number){
+  document.getElementById("portfolioBigPicIMG").src = "";
   document.getElementById("portfolioBigPicIMG").src = "images/portfolio/big/"+number+".jpg";
-
 
   document.getElementById("portfolioBigPic"). style.display = "block";
   document.getElementById("wholeSite").style.display = "none";
